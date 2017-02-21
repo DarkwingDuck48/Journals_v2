ab9e9353609100e3cb2e7cb9ba05058f187efa88
Max Britvin <maksbritvin@gmail.com>
2017-02-21 20:31:28 +0300

    Add func for mapping accounts started with 3

--- Tool.py
+++ Tool.py
@@ -41,15 +41,20 @@ def convert (filename):
 
 
 def acc3110101(sourceline):
+    """
+
+    :param sourceline: Line from journal with account 3110101
+    :return: Mapped line as string
+    """
     sline = sourceline
     ProdArr = PR_07 + PR_08 + PR_13 + ['PR_20', 'PR_67', "PR_17", "[None]"]
     PR_01020304 = ["PR_01", "PR_02", "PR_03"] + PR_04
     if len(sline) == 12:
         if sline[2] in ProdArr:                                                     # Line 2-8
             sline[0] = "3110001"
-        elif sline[2] in PR_01020304 and sline[4] in MK_03:                      # Line 9-12
+        elif sline[2] in PR_01020304 and sline[4] in MK_03:                         # Line 9-12
             sline[0] = "3110001"
-        elif sline[2] in PR_01020304 and sline[4] in MK09:                      # Line 18-41
+        elif sline[2] in PR_01020304 and sline[4] in MK09:                          # Line 18-41
             if sline[7] in ["PP_02", "PP_03", "PP_04"]:
                 sline[0] = "3110111"
             else:
@@ -62,10 +67,10 @@ def acc3110101(sourceline):
         elif sline[2] in ["PR_15", "PR_19"]:                                       # Line 47, 49
             sline[0] = "3110312"
             sline[2] = "PR_01"
-        elif sline[2] == "PR_16":
+        elif sline[2] == "PR_16":                                                  # Line 48
             sline[0] = "3110313"
             sline[2] = "PR_02"
-        elif sline[2] in PR_18:
+        elif sline[2] in PR_18:                                                    # Line 54
             sline[0] = "3110402"
             sline[2] = "PR_01"
         sline[7] = "[None]"
@@ -73,9 +78,9 @@ def acc3110101(sourceline):
     if len(sline) == 13:
         if sline[3] in ProdArr:
             sline[1] = "3110001"
-        elif sline[3] in PR_01020304 and sline[5] in MK_03:  # Line 9-12
+        elif sline[3] in PR_01020304 and sline[5] in MK_03:                        # Line 9-12
             sline[1] = "3110001"
-        elif sline[3] in PR_01020304 and sline[5] in MK09:  # Line 18-41
+        elif sline[3] in PR_01020304 and sline[5] in MK09:                         # Line 18-41
             if sline[8] in ["PP_02", "PP_03", "PP_04"]:
                 sline[1] = "3110111"
             else:
@@ -85,29 +90,106 @@ def acc3110101(sourceline):
                     sline[1] = "3110121"
                 elif sline[3] in PR_04:
                     sline[1] = "3110122"
-        elif sline[3] in ["PR_15", "PR_19"]:  # Line 47, 49
+        elif sline[3] in ["PR_15", "PR_19"]:                                       # Line 47, 49
             sline[1] = "3110312"
             sline[3] = "PR_01"
-        elif sline[3] == "PR_16":
+        elif sline[3] == "PR_16":                                                  # Line 48
             sline[1] = "3110313"
             sline[3] = "PR_02"
-        elif sline[3] in PR_18:
+        elif sline[3] in PR_18:                                                    # Line 54
             sline[1] = "3110402"
             sline[3] = "PR_01"
         sline[8] = "[None]"
+
     return ";".join(sline)+'\n'
 
 
 def acc1conv(sourceline):
+    """
+
+    :param sourceline: Line from journal with accounts "3110201", "3110202", "3110203", "3110204", "3110205"
+    :return: Mapped line as string
+    """
     sline = sourceline
-    sline = ";".join(sline)
-    return "{0}  CONVERTED by acc1conv+\n".format(sline)
+    if len(sline) == 12:
+        if sline[4] in MK_03:                                                   # Line 13-17
+             sline[0] = "3110001"
+        else:
+            if sline[0] == "3110201":
+                sline[0] = "3110122"
+            elif sline[0] == "3110204":
+                sline[0] = "3110125"
+            else:
+                sline[0] = "3110124"
+        sline[7] = "[None]"
+
+    elif len(sline) == 13:
+        if sline[5] in MK_03:                                                   # Line 13-17
+             sline[1] = "3110001"
+        else:
+            if sline[1] == "3110201":
+                sline[1] = "3110122"
+            elif sline[1] == "3110204":
+                sline[1] = "3110125"
+            else:
+                sline[1] = "3110124"
+        sline[8] = "[None]"
+
+    return ";".join(sline)+'\n'
 
 
 def acc2conv(sourceline):
+    """
+
+    :param sourceline: Line from journal with accounts "3110206", "3110207", "3110211", "3110212"
+    :return: Mappend line as string
+    """
     sline = sourceline
-    sline = ";".join(sline)
-    return "{0}  CONVERTED by acc2conv+\n".format(sline)
+    if len(sline) == 12:
+        if sline[0] == "3110206":                                               # Line 50-52
+            if sline[2] in ["PR_15","PR_19"]:
+                sline[0] = "3110312"
+                sline[2] = "PR_01"
+            if sline[2] == "PR_16":
+                sline[0] = "3110313"
+                sline[2] = "PR_02"
+        elif sline[0] == "3110207":
+            if sline[2] == "PR_15":
+                sline[0] = "3110311"
+                sline[2] = "PR_01"
+            elif sline[2] in PR_18:
+                sline[0] = "3110402"
+                sline[2] = "PR_05"
+        elif sline[0] == "3110211" and sline[2] in PR_18:
+            sline[0] = "3110401"
+            sline[2] = "PR_01"
+        elif sline[0] == "3110212" and sline[2] in PR_18:
+            sline[0] = "3110402"
+            sline[2] = "PR_01"
+        sline[7] = "[None]"
+    if len(sline) == 13:
+        if sline[1] == "3110206":                                               # Line 50-52
+            if sline[3] in ["PR_15","PR_19"]:
+                sline[1] = "3110312"
+                sline[3] = "PR_01"
+            if sline[3] == "PR_16":
+                sline[1] = "3110313"
+                sline[3] = "PR_02"
+        elif sline[1] == "3110207":
+            if sline[3] == "PR_15":
+                sline[1] = "3110311"
+                sline[3] = "PR_01"
+            elif sline[3] in PR_18:
+                sline[1] = "3110402"
+                sline[3] = "PR_05"
+        elif sline[1] == "3110211" and sline[3] in PR_18:
+            sline[1] = "3110401"
+            sline[3] = "PR_01"
+        elif sline[1] == "3110212" and sline[3] in PR_18:
+            sline[1] = "3110402"
+            sline[3] = "PR_01"
+        sline[8] = "[None]"
+    return ";".join(sline)+'\n'
 
 # Read easy accounts
 """
@@ -131,7 +213,7 @@ log = open('logs.txt', 'w', encoding="utf-8")
 
 # Open converted into txt journal
 starttime = time.time()
-with open("testJournal.txt", 'r', encoding="utf-8") as journal:
+with open("GRSHFM_Journal.txt", 'r', encoding="utf-8") as journal:
     for line in journal:
         if line.isspace():
             convertedJournals.write(line)
